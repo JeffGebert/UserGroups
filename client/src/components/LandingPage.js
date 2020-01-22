8e9e5d9bc223e5e10868758f6999432e30cd094e
Jeff Gebert <jeffgebert@hotmail.com>
2020-01-21 20:49:21 -0500

    added landingpage

---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./LandingPage.scss";
+import { Link } from "react-router-dom";
+
+
+export default function HomePage(props) {
+    return (
+        <div className="container">
+            <div className="links-container">
+            <Link to="users">
+              <button className="users-button">
+                Users
+              </button>
+            </Link>
+            <Link to="groups">
+            <button className="groups-button">
+                Groups
+              </button>
+            </Link>
+            </div>
+        </div>
+    )
+}
